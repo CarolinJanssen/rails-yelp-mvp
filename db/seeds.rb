4b084a0380b6a9a55659a11655c4a8b0e5232408
Carolin Janssen <c.j@hempberg.com>
2020-02-19 16:03:31 +0100

    mvp works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Company.name,
+    address: Faker::Food.fruits,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample,
+    phone_number: '1234567890'
+  )
+  restaurant.save!
+  p restaurant
+end
